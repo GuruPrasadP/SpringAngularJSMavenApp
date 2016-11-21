df73f3aaaf34c1257250b3e1190c71ef8d30f50e
ramreddykiran <ramreddykiran@gmail.com>
2016-11-21 18:40:07 +0530

    Able to run the application from REST client

--- src/main/java/com/user/controller/UserController.java
+++ src/main/java/com/user/controller/UserController.java
@@ -15,17 +15,17 @@ import com.user.service.UserService;
 
 @Controller
 @RequestMapping("/user")
-
 public class UserController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
-	
+
 	@Autowired
 	private UserService userService;
-	
-	@RequestMapping(value="/save", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public ResponseEntity<?> save(@RequestBody User user) {
+		logger.info("save operation is started for user " + user.getFirstName() + " " + user.getLastName());
 		return new ResponseEntity<String>("Saved employee details", HttpStatus.OK);
 	}
-
+	
 }
--- src/main/java/com/user/domain/User.java
+++ src/main/java/com/user/domain/User.java
@@ -1,5 +1,43 @@
 package com.user.domain;
 
+import java.util.List;
+
 public class User {
+	private String firstName;
+	private String lastName;
+	private String gender;
+	private List<Address> addresses;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public List<Address> getAddresses() {
+		return addresses;
+	}
+
+	public void setAddresses(List<Address> addresses) {
+		this.addresses = addresses;
+	}
 
 }
